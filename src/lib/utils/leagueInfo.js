41d74b7dfc5fe29820616eda508497f5d304e5da
Arrowhead29 <36304074+Arrowhead29@users.noreply.github.com>
2025-06-07 12:32:09 -0500

    Test - Avatars pulling from API

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,8 +32,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": null, // (optional)
       "bio": "No bio available",
-      "photo": "/managers/AdamBanning.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": null, // (optional) when did the manager start playing fantasy football
+      "photo": team.avatar, // square ratio recommended (no larger than 500x500)
       "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
