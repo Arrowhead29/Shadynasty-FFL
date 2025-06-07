661b82cd44e21d773c70bd32a891925b7eb77b38
Arrowhead29 <36304074+Arrowhead29@users.noreply.github.com>
2025-06-06 23:23:49 -0500

    Test

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -235,7 +235,7 @@ export const managers = [
     {
       "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "838155415312248832",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-      "name": "Wade Hageman",
+      "name": "Wade 3Hageman",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": null, // (optional)
       "bio": "No bio available",
