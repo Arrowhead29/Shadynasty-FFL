dfdcf9598c346fcf94182ea5c37765c4fad50d98
Arrowhead29 <36304074+Arrowhead29@users.noreply.github.com>
2025-06-06 23:44:42 -0500

    TEST

--- src/lib/Managers/Manager.svelte
+++ src/lib/Managers/Manager.svelte
@@ -224,7 +224,7 @@
 
 <div class="managerContainer">
     <div class="managerConstrained">
-        <img alt="team avatar" class="teamAvatar" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
+        <img alt="team avatar" class="managerPhoto" src="{team ? team.avatar : 'https://sleepercdn.com/images/v2/icons/player_default.webp'}" />
         <h2>
             {viewManager.name}
             <div class="teamSub">{coOwners ? 'Co-' : ''}<i>{getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}</i></div>
